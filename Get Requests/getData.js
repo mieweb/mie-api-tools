775d293f0779641a410535224b9e565c641dbe44
Maxwell Klema <maxklema@gmail.com>
2024-05-20 11:24:25 -0400

    GET requests functionality implemented

--- Get Requests/getData.js
+++ Get Requests/getData.js
@@ -15,47 +15,6 @@ function retrieveData(endpoint,fields, options){
 
     return result;
     
-    //determine the endpoint and parse appropriately
-    // switch(endpoint) {
-
-        // case "patients":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_conditions":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_conditions_family":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_targets":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_panel_status":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_partitions":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_partitions_restricted":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_procedures":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_clinical_restriction":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "patient_respiratordetails":
-        //     return makeQuery(endpoint, fields, options);
-
-        // case "order_items":
-        //     return makeQuery(endpoint, fields, options);
-
-        // default: //endpoint doesn't exist
-        //     throw new error.customError(error.ERRORS.INVALID_ENDPOINT, `The endpoint \"${endpoint}\" does not exist`);
-
-    //}
-    
 }
 
 module.exports = { retrieveData }
--- testing.js
+++ testing.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require('fs');
 
 const { URL, username, password } = require('./variables.js');
 const queryData = require('./Get Requests/getData.js');
@@ -7,5 +8,19 @@ URL.value = "https://maxprac.webchartnow.com/webchart.cgi";
 username.value = process.env.USERNAME;
 password.value = process.env.PASSWORD;
 
+console.log(queryData.retrieveData("", [], {  }));
+
 //console.log(queryData.retrieveData( "patients", ["first_name", "last_name", "ssn", "home_phone", "sex", "birth_date"], { first_name: "E"}));
-console.log(queryData.retrieveData( "order_types", [], { description: 'PT/OT' }));
+// endpoints = [ 
+
+// ];
+
+
+// for (i = 0; i < endpoints.length; i++){
+//     fs.appendFileSync('output.txt', endpoints[i]);
+//     fs.appendFileSync('output.txt', "\n");
+//     result = queryData.retrieveData( endpoints[i], [], {  });
+//     const jsonString = JSON.stringify(result);
+//     fs.appendFileSync('output.txt', jsonString.substring(0, 8000));
+//     fs.appendFileSync('output.txt', "\n");
+// }
