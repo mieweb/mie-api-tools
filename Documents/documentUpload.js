0194b47ad220401dc6ee62f522eee40f7b07e237
Maxwell Klema <maxklema@gmail.com>
2024-05-31 09:18:32 -0400

    Ability to upload images to WebChart

--- Documents/documentUpload.js
+++ Documents/documentUpload.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const { URL, practice } = require('../Variables/variables');
 const { parse } = require('csv-parse/sync');
 const log = require('../Logging/createLog');
+const FormData = require('form-data');
 
 //function for importing a single document
 function uploadSingleDocument(filename, storageType, docType, patID){
@@ -15,21 +16,31 @@ function uploadSingleDocument(filename, storageType, docType, patID){
 
         const mrnumber = `MR-${patID}`;
 
-        const data_request_params = {
-            'f': 'chart',
-            's': 'upload',
-            'storage_type': storageType,
-            'doc_type': docType,
-            'file': fs.readFileSync(filename),
-            'pat_id': patID,
-            'mrnumber': mrnumber,
-            'interface': 'WC_DATA_IMPORT'
-        }
+        const form = new FormData();
+        form.append('f', 'chart');
+        form.append('s', 'upload');
+        form.append('storage_type', storageType);
+        form.append('doc_type', docType);
+        form.append('file', fs.createReadStream(filename));
+        form.append('pat_id', patID);
+        form.append('mrnumber', mrnumber);
+        form.append('interface', 'WC_DATA_IMPORT');
+
+        // const data_request_params = {
+        //     'f': 'chart',
+        //     's': 'upload',
+        //     'storage_type': storageType,
+        //     'doc_type': docType,
+        //     'file': fs.readFileSync(filename),
+        //     'pat_id': patID,
+        //     'mrnumber': mrnumber,
+        //     'interface': 'WC_DATA_IMPORT'
+        // }
 
         log.createLog("info", `Document Upload Request:\nDocument Type: \"${docType}\"\nStorage Type: \"${storageType}\"\n Patient ID: ${patID}`);
-        axios.post(URL.value, data_request_params, {
+        axios.post(URL.value, form, {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded', //form encoded data
+                'Content-Type': 'multi-part/form-data', 
                 'cookie': `wc_miehr_${practice.value}_session_id=${cookie}`
             }
         })
